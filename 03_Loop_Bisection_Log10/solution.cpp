b3d0febd5fef57f424e2887338d4ccb9700f97bc
WeerawatW <weerawatwmt@gmail.com>
2023-08-14 20:59:48 +0700

    Add more solutions

--- 03_Loop_Bisection_Log10/solution.cpp
+++ 03_Loop_Bisection_Log10/solution.cpp
@@ -4,9 +4,18 @@
 using namespace std;
 
 int main(){
-    /*
-    in progress..
-    */
+    long double a, x;
+    cin >> a;
+    long double l=0,u=a;
+    x = l + (u - l)/2;
+    while (abs(a-pow(10,x)) > pow(10,-10)){
+        x = l + (u - l)/2;
+        if (pow(10,x) > a)
+            u = x;
+        else if (pow(10,x) < a)
+            l = x;
+    }
+    cout << x;
 
     return 0;
 }
