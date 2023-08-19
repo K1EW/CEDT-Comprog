7c40a8e8d08dbbfc573211e0bc18e07af5cf024b
WeerawatW <weerawatwmt@gmail.com>
2023-08-19 22:29:47 +0700

    modify solution

--- (PreQuiz)03_Loop_Snooker/solution.cpp
+++ (PreQuiz)03_Loop_Snooker/solution.cpp
@@ -9,53 +9,47 @@ int main(){
     while(n--){
         string s;
         getline(cin, s);
-        s += "   ";
-        int l = s.size(), lastRIdx = -1;
-        int score = 0;
-        bool ok = true;
-        for (int i=l-1;i>=0;i--){
-            if (s[i]=='R'){
-                lastRIdx = i;
-                break;
-            }
-        }
-        if (lastRIdx == -1){
-            cout << "WRONG_INPUT\n";
-            continue;
-        }
-        for (int i=0;i<=lastRIdx;i+=4){
-            ok = ok && (s[i]=='R' && s[i+2] != 'R');
-            if (s[i]=='R' || s[i+2]=='R') score += 1;
-            if (s[i]=='Y' || s[i+2]=='Y') score += 2;
-            if (s[i]=='G' || s[i+2]=='G') score += 3;
-            if (s[i]=='N' || s[i+2]=='N') score += 4;
-            if (s[i]=='B' || s[i+2]=='B') score += 5;
-            if (s[i]=='P' || s[i+2]=='P') score += 6;
-            if (s[i]=='K' || s[i+2]=='K') score += 7;
-        }
-        if (!ok){
-            cout << "WRONG_INPUT\n";
-            continue;
-        }
+        int l = s.size(), state = 0;
+        int cnt=0, score=0;
+        bool invalid = false;
         int j=0;
-        for (int i=lastRIdx+4;i<l-3;i+=2, j+=2){
-            if (j == (int)end.size()){
-                ok = false;
-                break;
-            }
-            ok = ok && (s[i]==end[j]);
-            if (s[i]=='R') score += 1;
-            if (s[i]=='Y') score += 2;
-            if (s[i]=='G') score += 3;
-            if (s[i]=='N') score += 4;
-            if (s[i]=='B') score += 5;
-            if (s[i]=='P') score += 6;
-            if (s[i]=='K') score += 7;
-        }
-        if (ok)
-            cout << score << "\n";
-        else
+        if (s[0] != 'R'){
             cout << "WRONG_INPUT\n";
+            continue;
+        }
+        for (int i=0;i<l;i+=2){
+            if (state == 0){
+                if (cnt%2==0 && s[i]=='R'){
+                    score += 1;
+                }else if (cnt%2==1 && s[i]!='R'){
+                    if (s[i] == 'Y') score += 2;
+                    if (s[i] == 'G') score += 3;
+                    if (s[i] == 'N') score += 4;
+                    if (s[i] == 'B') score += 5;
+                    if (s[i] == 'P') score += 6;
+                    if (s[i] == 'K') score += 7;
+                }else{
+                    state = 1;
+                    i -= 2;
+                }
+                cnt++;
+            }else{
+                if (j < end.size() && s[i] == end[j]){
+                    if (s[i] == 'Y') score += 2;
+                    if (s[i] == 'G') score += 3;
+                    if (s[i] == 'N') score += 4;
+                    if (s[i] == 'B') score += 5;
+                    if (s[i] == 'P') score += 6;
+                    if (s[i] == 'K') score += 7;
+                    j+=2;    
+                }else{
+                    invalid = true;
+                    break;
+                }
+            }
+        }
+        if (invalid) cout << "WRONG_INPUT\n";
+        else cout << score << "\n";
     }
 
     return 0;
