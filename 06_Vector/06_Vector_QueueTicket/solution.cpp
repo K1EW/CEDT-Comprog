d492aede741e780f72040b9e699dff30e3f8b680
WeerawatW <weerawatwmt@gmail.com>
2023-09-04 13:38:52 +0700

    add more solutions

--- 06_Vector/06_Vector_QueueTicket/solution.cpp
+++ 06_Vector/06_Vector_QueueTicket/solution.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-    vector<pair<int,int>> ctm;
+    vector<pair<int,int>> line;
+    vector<pair<int,int>> table;
     int n;
     int start;
     cin >> n;
+    int callCnt=0;
+    int total=0;
     while (n--){
         string cmd;
         int x;
         cin >> cmd;
         if (cmd == "next"){
-            cout << ">> call " << ctm[0].first << "\n"; 
+            cout << ">> call " << line[0].first << "\n";
+            table.push_back(line[0]);
+            line.erase(line.begin()); 
         }else if (cmd == "avg_qtime"){
-            
+            cout << ">> avg_qtime " << round((float)total/callCnt*100)/100 << "\n";
         }else{
             cin >> x;
             if (cmd == "reset")
                 start = x;
             else if (cmd == "new"){
                 cout << ">> ticket " << start << "\n";
-                ctm.push_back({start++, x});
+                line.push_back({start++, x});
             }
             else{
-                cout << ">> qtime " << ctm[0].first << " " << x-ctm[0].second << "\n";
-                ctm.erase(ctm.begin());
+                cout << ">> qtime " << table.back().first << " " << x-table.back().second << "\n";
+                total += x-table.back().second;
+                callCnt++;
+                table.pop_back();
             }
         }
     }
